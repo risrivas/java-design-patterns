6f974639bd4295ffa999c11e383bdda819d7b230
Rishi Srivastava <rishisrivasta@gmail.com>
2021-12-14 07:18:38 +0800

    liskov

---
+++ src/main/java/com/design/patterns/solid/liskov/Shape.java
@@ -0,0 +1,5 @@
+package com.design.patterns.solid.liskov;
+
+public interface Shape {
+    int computeArea();
+}
--- src/main/java/com/design/patterns/solid/liskov/Square.java
+++ src/main/java/com/design/patterns/solid/liskov/Square.java
@@ -1,24 +1,23 @@
 package com.design.patterns.solid.liskov;
 
-public class Square extends Rectangle {
-	
-	public Square(int side) {
-		super(side, side);
-	}
-	
-	@Override
-	public void setWidth(int width) {
-		setSide(width);
-	}
+public class Square implements Shape {
 
-	@Override
-	public void setHeight(int height) {
-		setSide(height);
-	}
+    private int side;
 
-	public void setSide(int side) {
-		super.setWidth(side);
-		super.setHeight(side);
-	}
+    public Square(int side) {
+        this.side = side;
+    }
 
+    public void setSide(int side) {
+        this.side = side;
+    }
+
+    public int getSide() {
+        return side;
+    }
+
+    @Override
+    public int computeArea() {
+        return side * side;
+    }
 }
